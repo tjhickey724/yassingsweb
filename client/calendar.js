1864917e88df766b59b5eafad94d7264320e38ec
tim <tjhickey@brandeis.edu>
2015-09-08 16:39:57 -0400

    first commit

---
+++ client/calendar.js
@@ -0,0 +1,13 @@
+var recentGigs =
+[{where:"The Jazz Lounge at Battery Wharf",  when:"4-5 September 2015", guests:"Yas Ishibashi and Ralph Pepe"},
+ {where:"Thelonious Monkfish",  when:"22 March 2015", guests:"Yas, Ishibashi, Jon Baboian and Bruce Gertz"},
+ {where:"Thelonious Monkfish",  when:"3 August 2014", guests:"Yas Ishibashi, Ralph Pepe, and Jon Wheatley"},
+ {where:"Thelonious Monkfish",  when:"22 December 2013", guests:"Yas Ishibashi, Ralph Pepe, Michael Carey"},
+ {where:"The Jazz Lounge at Battery Wharf",  when:"3 August 2013", guests:"Yas Ishibashi and Ralph Pepe"}
+
+
+]
+
+Template.calendar.helpers({
+	gigs:function(){return recentGigs;}
+})
