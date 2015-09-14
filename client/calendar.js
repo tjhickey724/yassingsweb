1ed925eea2cdb345abbb7afd5b701d0e9749f0bf
caitlin0528 <caitlin52895@icloud.com>
2015-09-14 13:07:09 -0400

    added mailing list

--- client/calendar.js
+++ client/calendar.js
@@ -1,5 +1,7 @@
 var recentGigs =
-[{where:"The Jazz Lounge at Battery Wharf",  when:"4-5 September 2015", guests:"Yas Ishibashi and Ralph Pepe"},
+[
+{where:"The Jazz Lounge at Battery Wharf",  when:"11-12 September 2015", guests:"Yas Ishibashi and Ralph Pepe"},
+{where:"The Jazz Lounge at Battery Wharf",  when:"4-5 September 2015", guests:"Yas Ishibashi and Ralph Pepe"},
  {where:"Thelonious Monkfish",  when:"22 March 2015", guests:"Yas, Ishibashi, Jon Baboian and Bruce Gertz"},
  {where:"Thelonious Monkfish",  when:"3 August 2014", guests:"Yas Ishibashi, Ralph Pepe, and Jon Wheatley"},
  {where:"Thelonious Monkfish",  when:"22 December 2013", guests:"Yas Ishibashi, Ralph Pepe, Michael Carey"},
---
+++ client/newsletter.js
@@ -0,0 +1,29 @@
+Template.newsletter.events({
+	'submit #Signup': function(event){
+		event.preventDefault();
+		var email= $("#email").val();
+		var birthdate= $("#birthdate").val();
+		var country= $("#country").val();
+		var zipcode= $("#zipcode").val();
+		var getemail= $("#getemail").is(':checked');
+		
+		var info={
+			email:email,
+			birthdate:birthdate,
+			country:country,
+			zipcode:zipcode,
+			getemail:getemail,
+		};
+		
+		
+		Mailinglist.insert(info);
+		console.dir(info);
+	}
+})
+
+
+Template.newsletter.helpers({
+	mailinfo: function(){
+		return Mailinglist.find();
+	}
+})
